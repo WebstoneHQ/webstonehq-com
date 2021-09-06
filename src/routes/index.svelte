420aa3b08cfb9d325cb145eb772b0d152a962fb1
Mike Nikles <mikenikles@pm.me>
2021-09-06 04:49:34 +0000

    Add first draft of the landing page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1 class="text-blue-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Dot from "$lib/dot.svelte";
+  import logo from "$lib/logo.svg";
+</script>
+
+<div class="h-screen w-screen bg-cover bg-no-repeat bg-center" style="background-image: url(/images/bg.jpg)">
+  <div class="container mx-auto pt-40 px-12">
+    <img src={logo} alt="Webstone Technologies logo" class="h-12 w-12" />
+    <h1 class="mt-24 font-semibold text-white text-3xl tracking-wider md:text-7xl">Webstone<br />Technologies<Dot /></h1>
+    <p class="mt-8 text-gray-400 tracking-wider md:text-2xl">Education<Dot /> Documentation<Dot /> Onboarding<Dot /></p>
+  </div>
+</div>
