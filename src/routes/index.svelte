75c4dd556718c162e23611a991c02d91ce87e0d2
Mike Nikles <mikenikles@pm.me>
2021-09-06 16:38:31 +0000

    Center the welcome section.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,8 @@
   import logo from "$lib/logo.svg";
 </script>
 
-<div class="h-screen w-screen bg-cover bg-no-repeat bg-center" style="background-image: url(/images/bg.jpg)">
-  <div class="container mx-auto pt-40 px-12">
+<div class="flex items-center h-screen w-screen bg-cover bg-no-repeat bg-center" style="background-image: url(/images/bg.jpg)">
+  <div class="container mx-auto px-12">
     <img src={logo} alt="Webstone Technologies logo" class="h-12 w-12" />
     <h1 class="mt-24 font-semibold text-white text-3xl tracking-wider md:text-7xl">Webstone<br />Technologies<Dot /></h1>
     <p class="mt-8 text-gray-400 tracking-wider md:text-2xl">Education<Dot /> Documentation<Dot /> Onboarding<Dot /></p>
