695c7e87867ca9eefbce63a147e198afdfb1f9ce
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2021-09-05 21:02:59 -0700

    Add Tailwind CSS (#7)
    * Auto-install Svelte & Tailwind VS Code extensions.
    
    * Add Tailwind CSS.
    
    * Configure SvelteKit HMR.

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,22 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess({
+        "postcss": true
+    })],
 
 	kit: {
 		adapter: vercel(),
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			server: {
+				hmr: {
+					clientPort: process.env.HMR_HOST ? 443 : 24678,
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+				}
+			}
+		}
 	}
 };
 
