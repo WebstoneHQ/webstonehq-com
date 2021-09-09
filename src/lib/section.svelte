759e503573ccc745fc99f734eee0d20cdd20fc29
Rock Hu <rockia@users.noreply.github.com>
2021-09-09 00:04:53 -0700

    Add basic scroll

--- src/lib/section.svelte
+++ src/lib/section.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   let clazz: string = "";
+  export let id;
 
   export { clazz as class};
 </script>
 
-<div class="pt-24 w-screen md:h-screen {clazz}">
+<div class="pt-24 w-screen md:h-screen {clazz}" {id}>
   <div class="flex items-center">
     <slot />
   </div>
